dda362e70a43eb3fe5a924ca2d26a33f5f431789
masterpiecekkk <48915913+masterpiecekkk@users.noreply.github.com>
2019-03-25 09:30:30 -0400

    Add files via upload

---
+++ ex4.py
@@ -0,0 +1,17 @@
+cars=100
+space_in-a_car=4.0
+drivers=30
+passnegers=90
+cars_not_driven=drivers
+cars_driven=drivers
+carpool_capacity=cars_driven_*space_in_a_car\
+average_passengers_per_=passengers/cars-driven
+
+
+print("There are",cars,"cars available.')
+print(There are only",drivers,"drivers avaliable.")
+print("There will be", cars_not_driven, "empty cars today.")
+print("We can transport", carpool_capacity, "people today.")
+print("We have", passengers, "to carpool today.")
+print("We need to put about", average_passengers_per_car,
+"in each car.")
